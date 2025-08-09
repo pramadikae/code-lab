a8cc62fbcd325f2f644f52265a4dc89019e0984f
pramadikae <dikaegamo1@gmail.com>
2025-08-09 15:32:43 +0700

    feat: add multiple comment for learn logical operators

--- 01_basic_python/13_logical_operators.py
+++ 01_basic_python/13_logical_operators.py
@@ -1,3 +1,15 @@
+"""
+Operator logika digunakan untuk menggabungkan dua 
+atau lebih kondisi. operator logika di python
+
+and: True jika kedua kondisi True
+or: True jika salah satu kondisi True
+not: Membalikan nilai boolean
+"""
+
+
+
+# contoh pengunaan
 umur = 25
 lulus = True
 print(umur > 18 and lulus)  # True
